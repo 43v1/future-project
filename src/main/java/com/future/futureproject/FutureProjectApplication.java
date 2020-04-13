ee7fd6e488bb312e127e8f8e120011b5065529d9
Antton Cholakov <antoncholakov43v1@gmail.com>
2020-04-13 21:40:49 +0300

    Initial commit

---
+++ src/main/java/com/future/futureproject/FutureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.future.futureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/future/futureproject/FutureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.futureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
