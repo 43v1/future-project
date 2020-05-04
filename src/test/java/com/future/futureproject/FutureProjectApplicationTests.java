b2084cd87f5061520ca7b1feff971aa484c87300
Antton Cholakov <antoncholakov43v1@gmail.com>
2020-05-04 18:54:19 +0300

    Created test configuration + test example

--- src/test/java/com/future/futureproject/FutureProjectApplicationTests.java
+++ src/test/java/com/future/futureproject/FutureProjectApplicationTests.java
@@ -1,13 +1,32 @@
 package com.future.futureproject;
 
+import com.future.futureproject.enums.Season;
+import com.future.futureproject.domain.model.SeasonModel;
+import com.future.futureproject.domain.model.TireModel;
+import com.future.futureproject.service.TireService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FutureProjectApplicationTests {
 
+    @Autowired
+    private TireService tireService;
+
     @Test
-    void contextLoads() {
+    void changeSeasonTest() {
+        TireModel tireModel = tireService.selectById(1);
+
+        SeasonModel summer = SeasonModel.builder().id(Season.SUMMER).build();
+        tireModel.setSeason(summer);
+
+        TireModel save = tireService.save(tireModel);
+
+        System.out.println(1);
     }
 
 }
